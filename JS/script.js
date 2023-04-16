4cb34474656d5b2f8304eb5ad6a19fceea72652a
KamCarrillo <92351526+KamCarrillo@users.noreply.github.com>
2023-04-16 00:56:55 -0600

    Maquina (#8)
    * Se agregó validación de LogIn, Admin
    
    * Administrador: HTML y CSS completo
    
    * Se agregó funcionalidad al panel
    
    * Se agregó funcionalidad al panel
    
    ---------
    
    Co-authored-by: Carlos Enrique <carlos.figueroa36422@outlook.com>
    Co-authored-by: SaChHe <samuel.chong@ingenieria.unam.edu>

--- JS/script.js
+++ JS/script.js
@@ -1,4 +1,7 @@
 const numeros = [];
+const data = ['01','02','03','04','05','06','07','08','09','10','11','12','13','14','15','16'];
+const price =[20.0,34.5,20.5];
+
 const panel = document.querySelector('.panel');
 const numerosList = document.getElementById('numeros');
 
@@ -11,17 +14,42 @@ panel.addEventListener('click', event => {
         imprimirHTML(numeros)
     } else if (value === 'C') {
         numeros.length = 0;
-        console.log('Clear');
-        clearConsole();
+        imprimirHTML(numeros)
     }
 });
 
 function imprimirHTML(numeros) {
     const pantalla = document.querySelector('.pantalla');
-    let html = '<p>';
-    numeros.forEach(numero => {
-        html += `${numero} `;
-    });
-    html += '</p>';
-    pantalla.innerHTML = html;
+    if(numeros.length>1){
+        let html = 'Producto: A ';
+        for(i=0;i<2;i++){
+            html += `${numeros[i]} `;
+        };
+        html += '<br>Costo: $';
+        p1=String(numeros[0])
+        p2=String(numeros[1])
+        code=p1+p2
+        for(i=0;i<4;i++){
+            if(code==data[i]){
+                html += `${price[0]} `;
+            }
+        };
+        for(i=4;i<8;i++){
+            if(code==data[i]){
+                html += `${price[1]} `;
+            }
+        };
+        for(i=8;i<16;i++){
+            if(code==data[i]){
+                html += `${price[2]} `;
+            }
+        };
+        html += '</br>';
+        numeros.length = 0;
+        pantalla.innerHTML = html;
+    }
+    else{
+        let html = 'Producto: <br>Costo: </br>';
+        pantalla.innerHTML = html;
+    }
 }
