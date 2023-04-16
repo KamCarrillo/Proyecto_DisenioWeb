2a973bfdefec250dca4415a42515d3dce3555d35
Kam Carrillo <santiago.carrilloc@ingenieria.unam.edu>
2023-04-15 23:18:00 -0600

    Se agregaron elementos esteticos y primera version funcional del script

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,27 @@
 const numeros = [];
-
 const panel = document.querySelector('.panel');
+const numerosList = document.getElementById('numeros');
+
 panel.addEventListener('click', event => {
     const value = event.target.innerText;
     if (!isNaN(value)) {
         numeros.push(Number(value));
-        console.log('Numbers:', numeros);
+        console.log(numeros);
     } else if (value === 'E') {
-        console.log('End');
+        imprimirHTML(numeros)
     } else if (value === 'C') {
         numeros.length = 0;
         console.log('Clear');
+        clearConsole();
     }
 });
 
-
-
+function imprimirHTML(numeros) {
+    const pantalla = document.querySelector('.pantalla');
+    let html = '<p>';
+    numeros.forEach(numero => {
+        html += `${numero} `;
+    });
+    html += '</p>';
+    pantalla.innerHTML = html;
+}
